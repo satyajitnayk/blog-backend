d3ebc23c17e075f81c77323f50a3f9c1d576d2f5
satyajit nayak <satyajitn15@gmail.com>
2022-10-24 22:48:51 +0530

    Add model mapper for mapping models b/w 2 classes.

--- src/main/java/com/satya/blog/services/impl/UserServiceImpl.java
+++ src/main/java/com/satya/blog/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.satya.blog.services.impl;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,9 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepo userRepo;
+	
+	@Autowired
+	private ModelMapper modelMapper;
 	
 	@Override
 	public UserDto createUser(UserDto userDto) {
@@ -73,24 +77,12 @@ public class UserServiceImpl implements UserService {
 	}
 	
 	private User dtoToUser(UserDto userDto) {
-		User user = new User();
-		user.setId(userDto.getId());
-		user.setEmail(userDto.getEmail());
-		user.setName(userDto.getName());
-		user.setPassword(userDto.getPassword());
-		user.setAbout(userDto.getAbout());
-		
+		User user = this.modelMapper.map(userDto, User.class);
 		return user;
 	}
 	
 	private UserDto userToDto(User user) {
-		UserDto userDto = new UserDto();
-		userDto.setId(user.getId());
-		userDto.setName(user.getName());
-		userDto.setEmail(user.getEmail());
-		userDto.setPassword(user.getPassword());
-		userDto.setAbout(user.getAbout());
-		
+		UserDto userDto = this.modelMapper.map(user, UserDto.class);
 		return userDto;
 	}
 
